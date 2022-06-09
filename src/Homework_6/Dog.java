4d0264ff90bbbd1f8a3fc8a124d2f84593446f28
Kush <kushnov@inbox.ru>
2022-06-09 13:24:51 +0300

    Homework 6

---
+++ src/Homework_6/Dog.java
@@ -0,0 +1,9 @@
+package Homework_6;
+
+public class Dog extends Animal{
+
+        public Dog(String animal, String name, float maxRun, float maxSwim) {
+            super(animal, name, maxRun, maxSwim);
+        }
+
+}
